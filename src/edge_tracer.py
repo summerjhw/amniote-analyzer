b54816093c7f361202e9584a682bb1be524b5ee3
summerjhw <summerjhw7@gmail.com>
2025-04-10 21:40:58 -0600

    move edge tracer to src folder

--- src/edge_tracer.py
+++ src/edge_tracer.py
@@ -3,7 +3,8 @@ import cv2
 # Read an image
 synapsid_filepath = "resources/images/synapsids/2_cropped_Therapsida_3.png"
 diapsid_filepath = "resources/images/diapsids/512px-Skull_diapsida_1.svg.png"
-img = cv2.imread(synapsid_filepath, cv2.IMREAD_GRAYSCALE)
+utaraptor_filepath = "resources/images/diapsids/cropped_BYU_Utahraptor_skeletal_mount.png"
+img = cv2.imread(utaraptor_filepath, cv2.IMREAD_GRAYSCALE)
 
 # Apply edge detection
 edges = cv2.Canny(img, threshold1=100, threshold2=200)
